51d138f6883b6d835fcf4812fd40140a466fa193
Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>
2023-04-05 00:38:51 +0300

    Add smoke-test protocol implementation
    Signed-off-by: Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use tokio::{net::TcpListener};
+
+const IP: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
+const PORT: u16 = 8080;
+
+mod smoke_test;
+use smoke_test::process;
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::new(IpAddr::V4(IP), PORT);
+
+    println!("Listening on {:?}:{:?}...", IP, PORT);
+
+    // Bind the listener to the address
+    let listener = TcpListener::bind(&addr)
+        .await
+        .unwrap();
+
+    loop {
+        println!("Waiting for connection ...");
+
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+
+        println!("Connection open\n");
+        // A new task is spawned for each inbound socket. The socket is
+        // moved to the new task and processed there.
+        let handle = tokio::spawn(async move {
+            // Do some async work
+            process(socket).await;
+        });
+
+        // Do some other work
+        let out = handle.await.unwrap();
+        println!("Connection closed\n");
+    }
+}
