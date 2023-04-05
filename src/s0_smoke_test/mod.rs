682962b4a700663d02b8576416478743e2f88c6c
Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>
2023-04-05 17:25:59 +0300

    Add fly.toml config and rename solutions
    Signed-off-by: Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>

--- src/s0_smoke_test/mod.rs
+++ src/s0_smoke_test/mod.rs
@@ -1,21 +1,18 @@
-
 use bytes::BytesMut;
 use std::io;
 use tokio::io::AsyncWriteExt;
-use tokio::{net::TcpStream, io::AsyncReadExt};
+use tokio::{io::AsyncReadExt, net::TcpStream};
 
-
-pub async fn process(mut socket: TcpStream) -> io::Result<usize> {
+pub async fn process(mut stream: TcpStream) -> io::Result<usize> {
     // The `Connection` lets us read/write redis **frames** instead of
     // byte streams. The `Connection` type is defined by mini-redis.
     let mut buf = BytesMut::with_capacity(100);
-    
-    while let Ok(len) = socket.read_buf(&mut buf).await {
+
+    while let Ok(len) = stream.read_buf(&mut buf).await {
         if len > 0 {
             println!("GOT {:?}\n", buf);
-            socket.write_buf(&mut buf).await;
-        }
-        else {
+            stream.write_buf(&mut buf).await;
+        } else {
             break;
         }
     }
